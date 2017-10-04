ddb81f1df108cb7961f2288b2385b9edf22decc3
Slava Juravliov <prospect.sjp@gmail.com>
2017-10-04 11:07:24 +0100

    Bean Naming,Contructor Injecting,Spring Container

--- com.tutorial.project/src/main/java/com/tutorial/project/App.java
+++ com.tutorial.project/src/main/java/com/tutorial/project/App.java
@@ -8,14 +8,22 @@ public class App {
     private Client client;
 	private EventLogger eventLogger;
 	
+	App(Client client, EventLogger eventLogger){
+		super();
+		this.client = client;
+		this.eventLogger = eventLogger;
+	};
+	
 	public static void main(String[] args) {
-		App app = new App();
 		
-		app.client = new Client("1", "John Smith");
+		// Create spring container object
+	    ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		
-		app.eventLogger = new ConsoleEventLogger();
+	    // Get bean from container
+	    App app = (App) ctx.getBean("app");
 		
 		app.logEvent("Some event for user 1");
+		app.logEvent("Some event for user 2");
 		}
 		
 	private void logEvent(String msg) {
